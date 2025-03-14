a994352844e13e5e5fe115838279327a4f14b7df
Anurag Pal <palanurag595@gmail.com>
2025-03-14 13:48:01 +0530

    Arithmatic_operators1.1

---
+++ python_basic.py
@@ -0,0 +1,7 @@
+#Arithmatic Operators
+a = 15
+b = 86
+print("Addition =", a+b)# Addition
+print("Subtraction", a-b)#subtraction
+print("Multiplication", a*b)#Multiplication
+print("Division", a/b)#Division
